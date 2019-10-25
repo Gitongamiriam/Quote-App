fa983579425bca9b5780e7d7716bb567de69ac07
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-10-25 15:59:57 +0300

    Add a description for quotes

--- src/app/qoutes/qoutes.component.ts
+++ src/app/qoutes/qoutes.component.ts
@@ -8,12 +8,12 @@ import{Qoutes} from '../qoutes';
 })
 export class QoutesComponent implements OnInit {
 qoutes:Qoutes[]=[
-  new Qoutes("Abraham lincoln", "Whatever you are be a good one", "",1),
-  new Qoutes("Albert Einstein", "If you want to live a happy life,tie to a goal,not to people or objects","",1),
-  new Qoutes("Ben Carson", "Uninformed people can be easily manipulated","",1),
-  new Qoutes("Gitonga Miriam","Work hard Untill your Bank Account balance looks like your mobile phone number","",1),
-  new Qoutes("Nick Vijucic","If i fail ,i try again ,and again","",1),
-  new Qoutes("Oprah Winfrey","Think like a Queen .A Queen is not afraid to fail to failure is another steppingstone to greatness","",1),
+  new Qoutes("Abraham lincoln", "Whatever you are be a good one", "explains how you should live in career wise",1),
+  new Qoutes("Albert Einstein", "If you want to live a happy life,tie to a goal,not to people or objects","explains the way of living of people ",1),
+  new Qoutes("Ben Carson", "Uninformed people can be easily manipulated","explains how ignorance it taking you",1),
+  new Qoutes("Gitonga Miriam","Work hard Untill your Bank Account balance looks like your mobile phone number","explains the way of living",1),
+  new Qoutes("Nick Vijucic","If i fail ,i try again ,and again","explains life lesson",1),
+  new Qoutes("Oprah Winfrey","Think like a Queen .A Queen is not afraid to fail to failure is another steppingstone to greatness","explains about failure ",1),
   
 
 ];
