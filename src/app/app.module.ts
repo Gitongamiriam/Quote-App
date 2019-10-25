40e81a5a80b35596268809d7d7a61ff0f5099a31
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-10-25 11:40:38 +0300

    Add a quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QoutesComponent } from './qoutes/qoutes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QoutesComponent
   ],
   imports: [
     BrowserModule,
