01e535c064eea66bdf5162a54020b295ad36c31b
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-10-28 14:38:08 +0300

    Create a directory for highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QoutesComponent } from './qoutes/qoutes.component';
 import { QoutesDetailComponent } from './qoutes-detail/qoutes-detail.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QoutesComponent,
-    QoutesDetailComponent
+    QoutesDetailComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
