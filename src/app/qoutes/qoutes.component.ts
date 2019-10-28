2d1124072c5d62ea7724dac964def9d93fea251d
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-10-28 14:58:39 +0300

    Add a date component

--- src/app/qoutes/qoutes.component.ts
+++ src/app/qoutes/qoutes.component.ts
@@ -9,7 +9,7 @@ import{Qoutes} from '../qoutes';
 export class QoutesComponent implements OnInit {
 qoutes:Qoutes[]=[
   new Qoutes("Abraham lincoln", "Whatever you are be a good one", "explains how you should live in career wise",new Date(2019,12,1)),
-  new Qoutes("Albert Einstein", "If you want to live a happy life,tie to a goal,not to people or objects","explains the way of living of people ",new Date(2013,11,)),
+  new Qoutes("Albert Einstein", "If you want to live a happy life,tie to a goal,not to people or objects","explains the way of living of people ",new Date(2013,11,5)),
   new Qoutes("Ben Carson", "Uninformed people can be easily manipulated","explains how ignorance it taking you",new Date(2040,12,2)),
   new Qoutes("Gitonga Miriam","Work hard Untill your Bank Account balance looks like your mobile phone number","explains the way of living",new Date(2012,3,2)),
   new Qoutes("Nick Vijucic","If i fail ,i try again ,and again","explains life lesson",new Date(2012,12,1)),
